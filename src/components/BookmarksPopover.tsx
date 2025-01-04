fe01a57e5a9f88decabd43137f7b71bf8e00b2da
Chirag Goyal <chirag.goyal777@gmail.com>
2025-01-04 15:33:34 +0530

    Empty bookmarks UI added

--- src/components/BookmarksPopover.tsx
+++ src/components/BookmarksPopover.tsx
@@ -2,13 +2,25 @@ import { forwardRef } from "react";
 import { useBookmarkContext } from "../hooks/useBookmarkContext";
 import JobList from "./JobList";
 import { createPortal } from "react-dom";
+import { BookmarkFilledIcon } from "@radix-ui/react-icons";
 
 const BookmarksPopover = forwardRef<HTMLDivElement>(function (_, ref) {
   const { bookmarkedJobItems, isLoading } = useBookmarkContext();
 
   return createPortal(
     <div ref={ref} className="bookmarks-popover">
-      <JobList jobItems={bookmarkedJobItems} isLoading={isLoading} source="bookmark" />
+      {bookmarkedJobItems.length === 0 ? (
+        <div className="bookmarks-popover__empty">
+          <BookmarkFilledIcon />
+          <p>Bookmarks will appear here</p>
+        </div>
+      ) : (
+        <JobList
+          jobItems={bookmarkedJobItems}
+          isLoading={isLoading}
+          source="bookmark"
+        />
+      )}
     </div>,
     document.body
   );
